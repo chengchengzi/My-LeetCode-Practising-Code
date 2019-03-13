3231b7c109971f28cf687821d438a83632f6effa
chengchengzi <sueannika@163.com>
2019-03-13 21:39:49 +0800

    update leetcode 167.cpp

--- array/167.cpp
+++ array/167.cpp
@@ -2,6 +2,10 @@
 #include<vector>
 using namespace std;
 
+
+/**
+     ±º‰∏¥‘”∂»£∫O£®n^2£©
+*/
 vector<int> twoSum(vector<int>& numbers,int target){
     vector<int> result(2,0);
     for(int i = 0; i < numbers.size();i++){
@@ -16,11 +20,41 @@ vector<int> twoSum(vector<int>& numbers,int target){
     return result;
 }
 
+vector<int> twoSum2(vector<int>& numbers,int target){
+    vector<int> result(2,0);
+    int i = 0;
+    int j = numbers.size() - 1;
+    while(i < j){
+        int sum = numbers[i] + numbers[j];
+        if(sum == target){
+            result[0] = i + 1;
+            result[1] = j + 1;
+            break;
+        }
+        else if(sum > target){
+            j--;
+        }
+        else{
+            i++;
+        }
+    }
+    return result;
+}
+
+
+
 int main(){
     vector<int> numbers = {2,7,11,15};
     int target = 9;
     vector<int> result = twoSum(numbers,target);
     for(auto w : result)
         cout << w << " ";
+
+    cout << endl;
+
+    vector<int> result2 = twoSum2(numbers,target);
+    for(auto w : result2)
+        cout << w << " ";
+
     return 0;
 }
